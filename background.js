67f2e64834e9c9114d7886ba9caef93a0c692e9b
Yash Desai <24643741+yhdesai@users.noreply.github.com>
2025-04-24 21:45:58 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     let url = new URL(details.url);
-    if (url.hostname.endsWith("twitter.com")) {
+    if (url.hostname.endsWith("x.com")) {
       return { redirectUrl: "https://wip.co" };
     }
   },
-  { urls: ["*://*.twitter.com/*"] },
+  { urls: ["*://*.x.com/*"] },
   ["blocking"]
 );
